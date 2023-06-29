a834ffbf8d664e47d7b428e10af9224405994085
mobinkazemi <s.mobinkazemi@gmail.com>
2023-06-29 14:03:31 +0330

    config - feature - environment variables added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'config/configuration';
 
+// console.log(require("dotenv").config())
 @Module({
+  imports: [ConfigModule.forRoot({envFilePath: `${process.env.NODE_ENV}.env`, load: [configuration]})],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT;
+  
+  await app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
 }
 bootstrap();
