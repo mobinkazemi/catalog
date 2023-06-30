32216f7f6dd1eb73a40a35bde4c79459225d71ed
mobinkazemi <s.mobinkazemi@gmail.com>
2023-06-30 11:18:47 +0330

    app - change - formatter code that now handles in formatterDto

--- src/common/dto/response-formatter.dto.ts
+++ src/common/dto/response-formatter.dto.ts
@@ -1,5 +1,26 @@
+import { ExecutionContext } from "@nestjs/common";
+import { DEFAULT_RESPONSE_CODE, DEFAULT_RESPONSE_MESSAGE } from "../constants";
+
 export class ResponseFormatterDto {
     statusCode?:number;
     message?:string;
     data?:any;
+
+    constructor(context:ExecutionContext, data:any){
+        let response: ResponseFormatterDto = {};
+
+        response.statusCode=
+          +context?.switchToHttp()?.getResponse()?.statusCode ||
+          DEFAULT_RESPONSE_CODE;
+
+        response.message = data?.message || data?.msg || DEFAULT_RESPONSE_MESSAGE;
+
+        delete data['message'];
+        delete data['msg'];
+        delete data['statusCode'];
+
+        response.data = data || {};
+
+        return response;
+    }
 }
--- src/common/interceptors/response-formatter.interceptor.ts
+++ src/common/interceptors/response-formatter.interceptor.ts
@@ -6,30 +6,13 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { DEFAULT_RESPONSE_CODE, DEFAULT_RESPONSE_MESSAGE } from '../constants';
 import { ResponseFormatterDto } from '../dto/response-formatter.dto';
 
 @Injectable()
 export class ResponseFormatterInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
-      map((data) => {
-        let response: ResponseFormatterDto = {};
-
-        response.statusCode=
-          +context?.switchToHttp()?.getResponse()?.statusCode ||
-          DEFAULT_RESPONSE_CODE;
-
-        response.message = data?.message || data?.msg || DEFAULT_RESPONSE_MESSAGE;
-
-        delete data['message'];
-        delete data['msg'];
-        delete data['statusCode'];
-
-        response.data = data || {};
-
-        return response;
-      }),
+      map((data) => new ResponseFormatterDto(context, data)),
     );
   }
 }
