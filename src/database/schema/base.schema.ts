30c0cde96f452c71c1bcc2ee761a687ecbc5fd7b
mobinkazemi <s.mobinkazemi@gmail.com>
2023-07-02 00:12:17 +0330

    baseSchema - fix - insertion validation bug

--- src/database/schema/base.schema.ts
+++ src/database/schema/base.schema.ts
@@ -1,13 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, ObjectId } from 'mongoose';
 @Schema({ id: true, timestamps: true })
 export abstract class Base {
-  @Prop({type:mongoose.Types.ObjectId, required: true})
-  _id:string;
-  @Prop({type:Date, required:true})
+  _id:ObjectId;
+  id:string;
   createdAt:Date;
-  @Prop({type:Date, required:true})
   updatedAt:Date
-  @Prop({ type: Date, required: false })
   deletedAt: Date;
 }
