e9352140cbdd5e4771ff094193a8ce7989f89537
mobinkazemi <s.mobinkazemi@gmail.com>
2023-07-20 20:49:03 +0330

    auth - feat - strategy files moved to separate folder

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [
-    ConfigModule,
-    UsersModule,
-    PassportModule,
-  ],
+  imports: [ConfigModule, UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy, JwtService, JwtStrategy],
   controllers: [AuthController],
 })
--- src/users/controller/admin.users.controller.ts
+++ src/users/controller/admin.users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, BadRequestException, UseGuards, ConflictException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  BadRequestException,
+  UseGuards,
+  ConflictException,
+} from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { CreateUserDto } from '../dto/request/create-user.dto';
 import { UpdateUserDto } from '../dto/request/update-user.dto';
@@ -6,11 +18,14 @@ import { FindUserDto } from '../dto/request/findone-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { FindUserResponseDto } from '../dto/response/findOne-user.dto';
 import { ResponseAfterCreateDto } from 'src/common/dto/response-after-create.dto';
-import { RolesGuard } from 'src/auth/roles.guard';
+import { RolesGuard } from 'src/auth/strategy/role.strategy';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesEnum } from 'src/common/enums/roles.enum';
 import { ApiProperty } from '@nestjs/swagger';
-import { FilterRequestUserDto , UserResponseListDto} from '../dto/request/filter-user.dto';
+import {
+  FilterRequestUserDto,
+  UserResponseListDto,
+} from '../dto/request/filter-user.dto';
 
 @Roles(RolesEnum.ADMIN)
 @UseGuards(RolesGuard)
@@ -19,18 +34,22 @@ import { FilterRequestUserDto , UserResponseListDto} from '../dto/request/filter
 export class UsersAdminController {
   constructor(private readonly usersService: UsersService) {}
 
-  @ApiProperty({type: CreateUserDto})
+  @ApiProperty({ type: CreateUserDto })
   @Post('create')
-  async create(@Body() createUserDto: CreateUserDto):Promise<ResponseAfterCreateDto> {
-    const exist =  await this.usersService.findOne({username:createUserDto.username})
-    if(exist) throw new ConflictException();
-    
+  async create(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<ResponseAfterCreateDto> {
+    const exist = await this.usersService.findOne({
+      username: createUserDto.username,
+    });
+    if (exist) throw new ConflictException();
+
     return await this.usersService.create(createUserDto, true);
   }
 
   @Get('list')
   async findAll(
-    @Query() data: FilterRequestUserDto
+    @Query() data: FilterRequestUserDto,
   ): Promise<Array<UserResponseListDto>> {
     return await this.usersService.findAll(data);
   }
