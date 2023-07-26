2f9fb55fc2e7cfa88badb5f3b590e25863f461a7
mobinkazemi <s.mobinkazemi@gmail.com>
2023-07-26 23:17:06 +0330

    application - fix - response formatter bug where with undefined data app crashes

--- src/common/dto/response-formatter.dto.ts
+++ src/common/dto/response-formatter.dto.ts
@@ -1,26 +1,33 @@
-import { ExecutionContext } from "@nestjs/common";
-import { DEFAULT_RESPONSE_CODE, DEFAULT_RESPONSE_MESSAGE } from "../constants/default-response.constant";
+import { ExecutionContext } from '@nestjs/common';
+import {
+  DEFAULT_RESPONSE_CODE,
+  DEFAULT_RESPONSE_MESSAGE,
+} from '../constants/default-response.constant';
 
 export class ResponseFormatterDto {
-    statusCode?:number;
-    message?:string;
-    data?:any;
+  statusCode?: number;
+  message?: string;
+  data?: any;
 
-    constructor(context:ExecutionContext, data:any){
-        let response: ResponseFormatterDto = {};
+  constructor(context: ExecutionContext, data: any) {
+    let response: ResponseFormatterDto = {};
 
-        response.statusCode=
-          +context?.switchToHttp()?.getResponse()?.statusCode ||
-          DEFAULT_RESPONSE_CODE;
-
-        response.message = data?.message || data?.msg || DEFAULT_RESPONSE_MESSAGE;
-
-        delete data['message'];
-        delete data['msg'];
-        delete data['statusCode'];
-
-        response.data = data || {};
-
-        return response;
+    if (data == undefined || data == null) {
+      data = {};
     }
+
+    response.statusCode =
+      +context?.switchToHttp()?.getResponse()?.statusCode ||
+      DEFAULT_RESPONSE_CODE;
+
+    response.message = data?.message || data?.msg || DEFAULT_RESPONSE_MESSAGE;
+
+    delete data['message'];
+    delete data['msg'];
+    delete data['statusCode'];
+
+    response.data = data;
+
+    return response;
+  }
 }
