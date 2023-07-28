27187c9811e40b2fcecb6535b3712bf87de88893
mobinkazemi <s.mobinkazemi@gmail.com>
2023-07-28 17:06:02 +0330

    application - style - response formatter interceptor prettied

--- src/common/interceptors/response-formatter.interceptor.ts
+++ src/common/interceptors/response-formatter.interceptor.ts
@@ -11,8 +11,8 @@ import { ResponseFormatterDto } from '../dto/response-formatter.dto';
 @Injectable()
 export class ResponseFormatterInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map((data) => new ResponseFormatterDto(context, data)),
-    );
+    return next
+      .handle()
+      .pipe(map((data) => new ResponseFormatterDto(context, data)));
   }
 }
