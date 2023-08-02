9d5d24ae3144cfb85e596f8375067819addc1bcc
mobinkazemi <s.mobinkazemi@gmail.com>
2023-08-02 16:34:15 +0330

    user - refactor - controller folder changed to controllers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './controller/users.controller';
+import { UsersController } from './controllers/users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User } from './schema/users.schema';
 import { UserSchema } from './schema/users.schema';
-import { UsersAdminController } from './controller/admin.users.controller';
+import { UsersAdminController } from './controllers/admin.users.controller';
 import { UsersRepository } from './users.repository';
 import { RolesService } from '../roles/roles.service';
 import { Role, RoleSchema } from '../roles/schema/roles.schema';
